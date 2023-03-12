453d3e9ff1ab6f0d14ea0e013acc18cd35e04973
Ingolfurinn <c.niccolo2000@gmail.com>
2023-03-12 15:58:39 +0100

    NewMono pt1 - fix compatibility invariants

--- Software/QuandleRUN/src/private.m
+++ Software/QuandleRUN/src/private.m
@@ -332,7 +332,7 @@ intrinsic internal_NewMonomorphism(A :: SeqEnum[SeqEnum[RngIntElt]], B :: SeqEnu
     for generator in genA do
         invs := internal_ElementInvariants(A,generator);
         for orbit in invImages do
-            if invs le orbit[2] then
+            if (&and [invs[i] le orbit[2][i] : i in [1..5]]) then
                 Images[generator] := orbit[1];
                 break;
             end if;
